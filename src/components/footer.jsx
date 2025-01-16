5c61680fde0ba44379c95fcafdd0cc989ebcb69e
Priya Pradhan <priy437pradhan@gmail.com>
2025-01-16 15:46:00 +0530

    datas added

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,7 +2,7 @@
 function footer() {
   return (
     <footer className="bg-gray-800 text-white text-center mt-16 py-4">
-    <p>&copy; {new Date().getFullYear()} Your Company Name. All rights reserved.</p>
+    <p>&copy; {new Date().getFullYear()} Sambit Ray. All rights reserved.</p>
   </footer>
   )
 }
